1adc6b5727e86b476d1460975526b060291d1549
Firdavs Ibodullaev <firdavs3253@gmail.com>
2022-05-30 21:43:43 +0500

    edited header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -15,15 +15,6 @@
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
         <!-- Notifications Dropdown Menu -->
-{{--        <li class="nav-item dropdown">--}}
-{{--            <a class="nav-link" data-toggle="dropdown" href="#">--}}
-{{--                <i class="far fa-bell"></i>--}}
-{{--            </a>--}}
-{{--            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">--}}
-{{--                <span class="dropdown-item dropdown-header">{{auth()->user()->full_name}}</span>--}}
-{{--                <div class="dropdown-divider"></div>--}}
-{{--            </div>--}}
-{{--        </li>--}}
         <li class="nav-item">
             <a class="nav-link" data-widget="fullscreen" href="#" role="button">
                 <i class="fas fa-expand-arrows-alt"></i>
